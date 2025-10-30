d5d74c06b7d1fe33d5c3960d8cfc9f752404329c
Becky <beckywong37.com>
2025-10-30 01:22:50 -0700

    Updated App.tsx for single page app. Includes navigation bar with links to various pages in our Secure Password Manager App

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* 
+This sets up the routes for the Secure Password Manager web app.
 
-function App() {
-  const [count, setCount] = useState(0)
+Features:
+- Uses React Router to navigate between single page apps
+- Navigation bar at the top with links to different pages:
+  - Password Generator, Login Page, Vault Page
+*/
 
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import GeneratorPage from './pages/Generator';
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div style={{ maxWidth: 900, margin: '20px auto', padding: 16, fontFamily: "sans-serif" }}>
+        <nav style={{ display: 'flex', gap: 20, marginBottom: 30, backgroundColor: 'lightgray', padding: 15, borderRadius: 8 }}>
+          <Link to="/login">Login Page</Link>
+          <Link to="/generator">Password Generator</Link>
+          <Link to="/vault">Vault Page</Link>
+        </nav>
+        <Routes>
+          <Route path="/login" element={<div>Login Page Under Construction</div>} />
+          <Route path="/generator" element={<GeneratorPage />} />
+          <Route path="/vault" element={<div>Vault Page Under Construction</div>} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+ 

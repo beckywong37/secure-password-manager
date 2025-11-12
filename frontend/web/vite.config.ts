1f0562570275ef84e658536601b07eae189d047a
curleyr <curleyr@oregonstate.edu>
2025-11-11 20:27:53 -0500

    adds dev proxy for auth endpoints

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -5,10 +5,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxy API requests to Django backend during development. only active when running pnp m dev
+      // Proxy API requests to Django backend during development. only active when running pnpm dev
       '/generator/api': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
+      },
+      '/auth/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
       },
     },
   },

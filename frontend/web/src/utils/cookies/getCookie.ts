a1e567815bcc815b7e2d043358a317e3f5b2a9bb
curleyr <curleyr@oregonstate.edu>
2025-11-14 09:42:38 -0500

    modifies cookie regex pattern and adds unit tests file

--- frontend/web/src/utils/cookies/getCookie.ts
+++ frontend/web/src/utils/cookies/getCookie.ts
@@ -5,6 +5,6 @@
 * @returns {string | null} string if cookie with matching name exists, otherwise null.
 */
 export const getCookie = (cookieName: string): string | null => {
-  const match = document.cookie.match(new RegExp(`${cookieName}=([^;\s]*)`));
+  const match = document.cookie.match(new RegExp(`(?:^|;\\s*)${cookieName}=([^;]*)`));
   return match ? decodeURIComponent(match[1]) : null;
 };
